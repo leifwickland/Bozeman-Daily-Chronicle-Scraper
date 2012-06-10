53822377f4eb327ec33cc11525997ce6197ed729
Leif Wickland <leifwickland@gmail.com>
2012-06-10 15:02:22 -0600

    If you send me encrypted content and send the key to decrypt it, I will.

--- BdcScraper.cs
+++ BdcScraper.cs
@@ -8,173 +8,238 @@ using System.Xml;
 using System.Collections.Generic;
 
 public class BdcScraper {
-    public static void Main(string[] args) {
-        if (args.Length != 4) {
-            PrintUsage();
-            return;
+  public static void Main(string[] args) {
+    if (args.Length != 4) {
+      PrintUsage();
+      return;
+    }
+
+    var rssUrl = args[0];
+    var toAddresses = args[1];
+    var fromAddress = args[2];
+    var mailServer = args[3];
+
+    PastArticles pastArticles = new PastArticles(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
+    Console.WriteLine("Requesting index from " + rssUrl);
+    var rss = GetUrl(rssUrl);
+    XmlDocument xml = new XmlDocument();
+    xml.LoadXml(WhackBadEntities(rss));
+    XmlNodeList items = xml.SelectNodes("//item");
+    Console.WriteLine("Found " + items.Count + " possible articles.");
+    foreach (XmlNode item in items) {
+      string link = item.SelectSingleNode("link").InnerText;
+      string title = item.SelectSingleNode("title").InnerText;
+      if (!link.Contains("bozemandailychronicle")) {
+        Console.WriteLine("Ignoring '{0}' from '{1}' since I only want articles from the BDC.", title, link);
+        continue;
+      }
+      if (!link.EndsWith(".html")) {
+        Console.WriteLine("Ignoring '{0}' from '{1}' since I only want HTML.", title, link);
+        continue;
+      }
+      if (link.Contains("/news/state/")) {
+        Console.WriteLine("Ignoring '{0}' from '{1}' since I don't want the Chronicle's state coverage.", title, link);
+        continue;
+      }
+      if (link.Contains("/poll_") || link.Contains("/youtube_") || link.Contains("/image_")) {
+        Console.WriteLine("Ignoring '{0}' from '{1}' since I don't want entries that are just polls, images or videos.", title, link);
+        continue;
+      }
+      if (!pastArticles.IsNew(link)) {
+        Console.WriteLine("Found '" + title + "' from " + link + " but ignoring it since it looks like old news.");
+        continue;
+      }
+      Console.WriteLine("Getting story '" + title + "' from " + link);
+      Story story = GetStory(link, title);
+      SendStory(story, toAddresses, fromAddress, mailServer);
+      pastArticles.Insert(link);
+      Console.WriteLine("Headline:");
+      Console.WriteLine(story.Headline);
+      Console.WriteLine("Body:");
+      Console.WriteLine(story.Body);
+      Console.WriteLine("^");
+      Console.WriteLine("End Body");
+      Console.WriteLine();
+      Console.WriteLine();
+    }
+  }
+
+  private static void PrintUsage() {
+    Console.WriteLine("USAGE <rssUrl> <toEmailAddresses> <fromEmailAddress> <mailServer>");
+    Console.WriteLine("");
+  }
+
+  private static string WhackBadEntities(string xml) {
+    return Regex.Replace(xml, @"&(?!(quot|amp|apos|lt|gt|#(\d{1,5}|x[\da-fA-F]{1,4}));)(.|$)", @"&amp;$3", RegexOptions.Multiline);
+  }
+
+  private static string Deentityize(string s) {
+    return s.Replace("&#036;", "$").Replace("&#36;", "$").Replace("&#8217;", "'").Replace("&#8220;", "\"").Replace("&#8221;", "\"").Replace("&rsquo;", "'").Replace("&lsquo;", "'");
+  }
+
+  private static string GetUrl(string url) {
+    WebRequest request = WebRequest.Create(url);
+    Stream output = request.GetResponse().GetResponseStream();
+    string page = new StreamReader(output).ReadToEnd();
+    return page;
+  }
+
+  private static Story GetStory(string url, string headline) {
+    Story story = new Story();
+    story.Url = url;
+    story.Headline = Deentityize(headline);
+    story.Body = string.Format("<a href='{0}'>{1}</a><br>{2}", url, headline, Deentityize(ExtractStoryBodyFrom(GetUrl(url), url)));
+    return story;
+  }
+
+  private static void SendStory(Story story, string toAddresses, string fromAddress, string mailServer) {
+    MailMessage message = new MailMessage(fromAddress, toAddresses, string.Format("BozemanDailyChronicle: {0}" , story.Headline), story.Body);
+    message.IsBodyHtml = true;
+    SmtpClient client = new SmtpClient(mailServer);
+    client.Credentials = null;
+    client.Send(message);
+  }
+
+  private enum ParseState {
+    LookingForImages,
+    LookingForGalleryOrHeadline,
+    ReadingGallery,
+    ReadingHeadline,
+    LookingForContent,
+    LookingForContentEnd,
+    LookingForEncrypted,
+    ReadingEncrypted,
+  }
+
+  private static void AddLine(string line, StringBuilder body) {
+    line = Regex.Replace(line, "&#822[01]([^;])", delegate(Match match) { return match.Groups[1].Value; }).Trim();
+    Console.WriteLine("    In state " + state + " adding line: " + line);
+    body.AppendLine(line);
+  }
+
+  private static string DecryptLine(string line) {
+    string unescaped = line.Trim().Replace("&gt;", ">").Replace("&lt;", "<").Replace("&amp;", "&");
+    if (!unescaped.StartsWith("kAm")) {
+      return ("<p><b>Couldn't figure out how to decrypt: " + line + "</b></p>");
+    }
+    StringBuilder b = new StringBuilder();
+    foreach (char c in unescaped) {
+      if (c < '!' || c > '~') {
+        b.Append(c);
+      } else {
+        b.Append(DecryptChar(c));
+      }
+    }
+    return b.ToString();
+  }
+
+  private static char DecryptChar(char c) {
+    return (char)(((c + 14) % 94) + 33);
+  }
+
+  private static ParseState state;
+
+  private static ParseState Transition(ParseState newState, string line) {
+    Console.WriteLine("Transitioning to " + newState + " on line: " + line);
+    state = newState;
+    return newState;
+  }
+
+  private static string ExtractStoryBodyFrom(string page, string url) {
+    Transition(ParseState.LookingForGalleryOrHeadline, "BeginningOfStory");
+    StringBuilder body = new StringBuilder();
+    using (StringReader reader = new StringReader(page)) {
+      string line;
+      while ((line = reader.ReadLine()) != null) {
+        switch (state) {
+        case ParseState.LookingForGalleryOrHeadline:
+          if (line.Contains("-- AP Bookmark --")) {
+            Transition(ParseState.ReadingHeadline, line);
+          } else if (line.Contains("class=\"instant-gallery\"")) {
+            Transition(ParseState.ReadingGallery, line);
+          }
+          break;
+        case ParseState.ReadingGallery:
+          if (line.Contains("class=\"preview-slide-navigator\"")) {
+            AddLine("<br><br>", body);
+            Transition(ParseState.LookingForGalleryOrHeadline, line);
+          } else if (line.Contains("<img")) {
+            AddLine(line, body);
+          }
+          break;
+        case ParseState.ReadingHeadline:
+          AddLine(line, body);
+          if(line.Contains("class=\"author ")) {
+            AddLine("<br><br>", body);
+            Transition(ParseState.LookingForContent, line);
+          }
+          break;
+        case ParseState.LookingForContent:
+          if (line.Contains("class=\"content\"")) {
+            Transition(ParseState.LookingForContentEnd, line);
+          }
+          break;
+        case ParseState.LookingForContentEnd:
+          if (line.Contains("tncms-restricted-notice") || line.Contains("subscription-notice")) {
+            Transition(ParseState.LookingForEncrypted, line);
+          } else {
+            AddLine(line, body);
+          }
+          break;
+        case ParseState.LookingForEncrypted:
+          if (line.Contains("class=\"encrypted-content\"")) {
+            Transition(ParseState.ReadingEncrypted, line);
+          }
+          break;
+        case ParseState.ReadingEncrypted:
+          if (line.Contains("</div>")) {
+            Transition(ParseState.LookingForEncrypted, line);
+          } else {
+            AddLine(DecryptLine(line), body);
+          }
+          break;
         }
-
-        var rssUrl = args[0];
-        var toAddresses = args[1];
-        var fromAddress = args[2];
-        var mailServer = args[3];
-
-        PastArticles pastArticles = new PastArticles(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
-        Console.WriteLine("Requesting index from " + rssUrl);
-        var rss = GetUrl(rssUrl);
-        XmlDocument xml = new XmlDocument();
-        xml.LoadXml(WhackBadEntities(rss));
-        XmlNodeList items = xml.SelectNodes("//item");
-        Console.WriteLine("Found " + items.Count + " possible articles.");
-        foreach (XmlNode item in items) {
-            string link = item.SelectSingleNode("link").InnerText;
-            string title = item.SelectSingleNode("title").InnerText;
-            if (!link.EndsWith(".html")) {
-                Console.WriteLine("Ignoring '{0}' from '{1}' since I only want HTML.", title, link);
-                continue;
-            }
-            if (link.Contains("/news/state/")) {
-                Console.WriteLine("Ignoring '{0}' from '{1}' since I don't want the Chronicle's state coverage.", title, link);
-                continue;
-            }
-            if (link.Contains("/youtube_") || link.Contains("/image_")) {
-                Console.WriteLine("Ignoring '{0}' from '{1}' since I don't want entries that are just images or videos.", title, link);
-                continue;
-            }
-            if (!pastArticles.IsNew(link)) {
-                Console.WriteLine("Found '" + title + "' from " + link + " but ignoring it since it looks like old news.");
-                continue;
-            }
-            Console.WriteLine("Getting story '" + title + "' from " + link);
-            Story story = GetStory(link, title);
-            SendStory(story, toAddresses, fromAddress, mailServer);
-            pastArticles.Insert(link);
-            Console.WriteLine(story.Headline);
-            Console.WriteLine(story.Body);
-            Console.WriteLine();
-            Console.WriteLine();
-        }
+      }
     }
-
-    private static void PrintUsage() {
-        Console.WriteLine("USAGE <rssUrl> <toEmailAddresses> <fromEmailAddress> <mailServer>");
-        Console.WriteLine("");
+    string capturedBody = body.ToString();
+    if (capturedBody.Length < 100) {
+      throw new Exception("Didn't parse correctly: " + url + " " + state + "\n" + page);
     }
+    return capturedBody;
+  }
 
-    private static string WhackBadEntities(string xml) {
-        return Regex.Replace(xml, @"&(?!(quot|amp|apos|lt|gt|#(\d{1,5}|x[\da-fA-F]{1,4}));)(.|$)", @"&amp;$3", RegexOptions.Multiline);
-    }
-
-    private static string Deentityize(string s) {
-        return s.Replace("&#036;", "$").Replace("&#36;", "$").Replace("&#8217;", "'").Replace("&#8220;", "\"").Replace("&#8221;", "\"").Replace("&rsquo;", "'").Replace("&lsquo;", "'");
-    }
-
-    private static string GetUrl(string url) {
-        WebRequest request = WebRequest.Create(url);
-        Stream output = request.GetResponse().GetResponseStream();
-        string page = new StreamReader(output).ReadToEnd();
-        return page;
-    }
-
-    private static Story GetStory(string url, string headline) {
-        Story story = new Story();
-        story.Url = url;
-        story.Headline = Deentityize(headline);
-        story.Body = string.Format("<a href='{0}'>{1}</a><br>{2}", url, headline, Deentityize(ExtractStoryBodyFrom(GetUrl(url), url)));
-        return story;
-    }
-
-    private static void SendStory(Story story, string toAddresses, string fromAddress, string mailServer) {
-        MailMessage message = new MailMessage(fromAddress, toAddresses, string.Format("BozemanDailyChronicle: {0}" , story.Headline), story.Body);
-        message.IsBodyHtml = true;
-        SmtpClient client = new SmtpClient(mailServer);
-        client.Credentials = null;
-        client.Send(message);
-    }
-
-    private enum ParseState {
-        LookingForHeadline,
-            FoundHeadline,
-            SkippingScript,
-            LookingForEnd,
-    }
-
-    private static void AddLine(string line, StringBuilder body) {
-        line = Regex.Replace(line, "&#822[01]([^;])", delegate(Match match) { return match.Groups[1].Value; });
-        body.AppendLine(line.Trim());
-    }
-
-    private static string ExtractStoryBodyFrom(string page, string url) {
-        StringBuilder body = new StringBuilder();
-        ParseState state = ParseState.LookingForHeadline;
-        using (StringReader reader = new StringReader(page)) {
-            string line;
-            while ((line = reader.ReadLine()) != null) {
-                switch (state) {
-                case ParseState.LookingForHeadline:
-                    if (line.Contains("byline")) {
-                        AddLine(line, body);
-                        state = ParseState.LookingForEnd;
-                    }
-                    break;
-                case ParseState.LookingForEnd:
-                    if (line.Contains("-- begin comment tabs --") || line.Contains("blox-comments") || line.Contains("<!-- begin comment tabs") || line.Contains("<!-- bottom html") || line.Contains("story-tools-sprite")) {
-                        return body.ToString();
-                    }
-                    if (line.Contains("<script")) {
-                        if (line.Contains("</script")) {
-                            state = ParseState.LookingForEnd;
-                        }
-                        else {
-                            state = ParseState.SkippingScript;
-                        }
-                        break;
-                    }
-                    AddLine(line, body);
-                    break;
-                case ParseState.SkippingScript:
-                    if (line.Contains("</script")) {
-                        state = ParseState.LookingForEnd;
-                    }
-                    break;
-                }
-            }
-        }
-        throw new Exception("Didn't parse correctly: " + url + " " + state + "\n" + page);
-    }
-
-    private class Story {
-        public string Url;
-        public string Headline;
-        public string Body;
-    }
+  private class Story {
+    public string Url;
+    public string Headline;
+    public string Body;
+  }
 }
 
 class PastArticles {
-    public PastArticles(string path) {
-        historyPath = path + Path.DirectorySeparatorChar + "PastArticles.history";
-        if (historyPath.StartsWith("file:"))
-            historyPath = historyPath.Substring(5);
-        Console.WriteLine("History path: " + historyPath);
-        if (!File.Exists(historyPath)) {
-            using (File.Create(historyPath)) {
-                // nothing!
-            }
-        }
-        list = new HashSet<string>(File.ReadAllLines(historyPath));
+  public PastArticles(string path) {
+    historyPath = path + Path.DirectorySeparatorChar + "PastArticles.history";
+    if (historyPath.StartsWith("file:"))
+      historyPath = historyPath.Substring(5);
+    Console.WriteLine("History path: " + historyPath);
+    if (!File.Exists(historyPath)) {
+      using (File.Create(historyPath)) {
+        // nothing!
+      }
     }
+    list = new HashSet<string>(File.ReadAllLines(historyPath));
+  }
 
-    private string historyPath;
-    private HashSet<string> list;
+  private string historyPath;
+  private HashSet<string> list;
 
-    public bool IsNew(string url) {
-        return !list.Contains(url);
+  public bool IsNew(string url) {
+    return !list.Contains(url);
+  }
+
+  public void Insert(string url) {
+    if (!list.Contains(url)) {
+      list.Add(url);
+      File.AppendAllText(historyPath, url + "\n");
     }
-
-    public void Insert(string url) {
-        if (!list.Contains(url)) {
-            list.Add(url);
-            File.AppendAllText(historyPath, url + "\n");
-        }
-    }
+  }
 }
